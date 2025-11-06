6492fe23d93407d6b7dab63377961fcebe3a8e41
SvetlanaKozlova0 <lana.kozlova2005@list.ru>
2025-11-06 23:32:53 +0300

    add cipher modes
    cfb mode, ofb mode, ctr mode, random delta mode
    
    need to think more about random delta

--- CryptoLabs/CryptoLabs/Utility/CipherModes/CipherMode.cs
+++ CryptoLabs/CryptoLabs/Utility/CipherModes/CipherMode.cs
@@ -172,12 +172,54 @@ public class CFBMode : ICipherMode
 {
     public byte[] Encrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        if (data.Length % BlockSize != 0)
+        {
+            throw new ArgumentException("Data must be multiple of BlockSize");
+        }
+        if (iv.Length != BlockSize)
+        {
+            throw new ArgumentException($"IV must be {BlockSize} bytes");
+        }
+        byte[] resultData = new byte[data.Length];
+        byte[] plainText = new byte[BlockSize];
+        Array.Copy(iv, plainText, BlockSize);
+        for (int i = 0; i < data.Length; i += BlockSize)
+        {
+            byte[] encryptedPlainText = cipher.Encrypt(plainText);
+            byte[] currentBlock = new byte[BlockSize];
+            Array.Copy(data, i, currentBlock, 0, BlockSize);
+            byte[] encrypted = BitFunctions.XorBlocks(currentBlock, encryptedPlainText, BlockSize);
+            Array.Copy(encrypted, 0, resultData, i, BlockSize);
+            Array.Copy(encrypted, plainText, BlockSize);
+        }
+
+        return resultData;
     }
 
     public byte[] Decrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        if (data.Length % BlockSize != 0)
+        {
+            throw new ArgumentException("Data must be multiple of BlockSize");
+        }
+        if (iv.Length != BlockSize)
+        {
+            throw new ArgumentException($"IV must be {BlockSize} bytes");
+        }
+        byte[] resultData = new byte[data.Length];
+        byte[] plainText = new byte[BlockSize];
+        Array.Copy(iv, plainText, BlockSize);
+        for (int i = 0; i < data.Length; i += BlockSize)
+        {
+            byte[] encryptedPlainText = cipher.Encrypt(plainText);
+            byte[] current = new byte[BlockSize];
+            Array.Copy(data, i, current, 0, BlockSize);
+            byte[] decrypted = BitFunctions.XorBlocks(current, encryptedPlainText, BlockSize);
+            Array.Copy(decrypted, 0, resultData, i, BlockSize);
+            Array.Copy(current, plainText, BlockSize);
+        }
+
+        return resultData;
     }
     public CipherMode Mode => CipherMode.CFB;
     public int BlockSize => 8;
@@ -187,12 +229,33 @@ public class OFBMode : ICipherMode
 {
     public byte[] Encrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        if (data.Length % BlockSize != 0)
+        {
+            throw new ArgumentException("Data must be multiple of BlockSize");
+        }
+        if (iv.Length != BlockSize)
+        {
+            throw new ArgumentException($"IV must be {BlockSize} bytes");
+        }
+        byte[] resultData = new byte[data.Length];
+        byte[] plainText = new byte[BlockSize];
+        Array.Copy(iv, plainText, BlockSize);
+        for (int i = 0; i < data.Length; i += BlockSize)
+        {
+            byte[] encryptedPlainText = cipher.Encrypt(plainText);
+            byte[] current = new byte[BlockSize];
+            Array.Copy(data, i, current, 0, BlockSize);
+            byte[] xored = BitFunctions.XorBlocks(current, encryptedPlainText, BlockSize);
+            Array.Copy(xored, 0, resultData, i, BlockSize);
+            Array.Copy(encryptedPlainText, plainText, BlockSize);
+        }
+
+        return resultData;
     }
 
     public byte[] Decrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        return Encrypt(cipher, data, iv);
     }
     public CipherMode Mode => CipherMode.OFB;
     public int BlockSize => 8;
@@ -200,29 +263,99 @@ public class OFBMode : ICipherMode
 
 public class CTRMode : ICipherMode
 {
+    private void IncrementCounter(byte[] counter)
+    {
+        for (int i = counter.Length - 1; i >= 0; i--)
+        {
+            if (++counter[i] != 0)
+            {
+                break;
+            }
+        }
+    }
     public byte[] Encrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        if (data.Length % BlockSize != 0)
+        {
+            throw new ArgumentException("Data must be multiple of BlockSize");
+        }
+        if (iv.Length != BlockSize)
+        {
+            throw new ArgumentException($"IV must be {BlockSize} bytes");
+        }
+        byte[] resultData = new byte[data.Length];
+        byte[] counter = new byte[BlockSize];
+        Array.Copy(iv, counter, BlockSize);
+
+        for (int i = 0; i < data.Length; i += BlockSize)
+        {
+            byte[] encryptedPlainText = cipher.Encrypt(counter);
+            byte[] current = new byte[BlockSize];
+            Array.Copy(data, i, current, 0, BlockSize);
+
+            byte[] xored = BitFunctions.XorBlocks(current, encryptedPlainText, BlockSize);
+            Array.Copy(xored, 0, resultData, i, BlockSize);
+            IncrementCounter(counter);
+        }
+
+        return resultData;
     }
 
+    
     public byte[] Decrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        return Encrypt(cipher, data, iv);
     }
     public CipherMode Mode => CipherMode.CTR;
     public int BlockSize => 8;
 }
 
+//need to think about design to keep deltas
 public class RandomDeltaMode : ICipherMode
 {
+    private readonly Random _random = new Random();
+    public List<byte[]> Deltas { get; } = new List<byte[]>();
     public byte[] Encrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        if (data.Length % BlockSize != 0)
+        {
+            throw new ArgumentException("Data must be multiple of BlockSize");
+        }
+        byte[] resultData = new byte[data.Length];
+        Deltas.Clear();
+        for (int i = 0; i < data.Length; i += BlockSize)
+        {
+            byte[] current = new byte[BlockSize];
+            Array.Copy(data, i, current, 0, BlockSize);
+            byte[] newDelta = new byte[BlockSize];
+            _random.NextBytes(newDelta);
+            Deltas.Add(newDelta);
+            byte[] xored = BitFunctions.XorBlocks(current, newDelta, BlockSize);
+            byte[] encrypted = cipher.Encrypt(xored);
+            Array.Copy(encrypted, 0, resultData, i, BlockSize);
+        }
+
+        return resultData;
     }
 
     public byte[] Decrypt(ISymmetricCipher cipher, byte[] data, byte[] iv)
     {
-        throw new NotImplementedException();
+        if (data.Length % BlockSize != 0)
+        {
+            throw new ArgumentException("Data must be multiple of BlockSize");
+        }
+        byte[] resultData = new byte[data.Length];
+        for (int i = 0; i < data.Length; i += BlockSize)
+        {
+            byte[] current = new byte[BlockSize];
+            Array.Copy(data, i, current, 0, BlockSize);
+            byte[] decrypted = cipher.Decrypt(current);
+            byte[] delta = Deltas[i / BlockSize];
+            byte[] original = BitFunctions.XorBlocks(decrypted, delta, BlockSize);
+            Array.Copy(original, 0, resultData, i, BlockSize);
+        }
+
+        return resultData;
     }
     public CipherMode Mode => CipherMode.RandomDelta;
     public int BlockSize => 8;
