b5e8cd2a7e5414a6d679045aeb9ae07865517e4b
SvetlanaKozlova0 <lana.kozlova2005@list.ru>
2025-11-12 18:51:34 +0300

    deal done, need testing

--- CryptoLabs/CryptoLabs/DEAL/DealAlgorithm.cs
+++ CryptoLabs/CryptoLabs/DEAL/DealAlgorithm.cs
@@ -4,18 +4,64 @@ namespace CryptoLabs.DEAL;
 
 public class DealAlgorithm: ISymmetricCipher
 {
+    private byte[][] _roundKeys;
+    private readonly DealEncryptionRound _encryptionRound = new();
+    
     public byte[] Encrypt(byte[] inputBlock)
     {
-        throw new NotImplementedException();
+        if (inputBlock.Length != 16)
+        {
+            throw new ArgumentException(
+                $"Length of input block for DEAL must be 16 bytes, but got {inputBlock.Length}");
+        }
+        byte[] left = new byte[8];
+        byte[] right = new byte[8];
+        
+        Array.Copy(inputBlock, 0, left, 0, 8);
+        Array.Copy(inputBlock, 8, right, 0, 8);
+
+        int rounds = _roundKeys.Length;
+        for (int i = 0; i < rounds; i++)
+        {
+            byte[] temp = left;
+            left = right;
+            byte[] fResult = _encryptionRound.PerformEncryptConversion(right, _roundKeys[i]);
+            right = BitFunctions.XorBlocks(temp, fResult, 8);
+        }
+
+        byte[] output = new byte[16];
+        Array.Copy(left, 0, output, 0, 8);
+        Array.Copy(right, 0, output, 8, 8);
+        return output;
     }
 
     public byte[] Decrypt(byte[] inputBlock)
     {
-        throw new NotImplementedException();
+        if (inputBlock.Length != 16)
+        {
+            throw new ArgumentException(
+                $"Length of input block for DEAL must be 16 bytes, but got {inputBlock.Length}");
+        }
+        byte[] left = new byte[8];
+        byte[] right = new byte[8];
+        Array.Copy(inputBlock, 0, left, 0, 8);
+        Array.Copy(inputBlock, 8, right, 0, 8);
+        int rounds = _roundKeys.Length;
+        for (int i = rounds - 1; i >= 0; i--)
+        {
+            byte[] temp = right;
+            right = left;
+            byte[] fResult = _encryptionRound.PerformEncryptConversion(left, _roundKeys[i]);
+            left = BitFunctions.XorBlocks(temp, fResult, 8);
+        }
+        byte[] output = new byte[16];
+        Array.Copy(left, 0, output, 0, 8);
+        Array.Copy(right, 0, output, 8, 8);
+        return output;
     }
 
     public void SetRoundKeys(byte[][] roundKeys)
     {
-        throw new NotImplementedException();
+        _roundKeys = roundKeys;
     }
 }
--- CryptoLabs/CryptoLabs/DEAL/DealEncryptionRound.cs
+++ CryptoLabs/CryptoLabs/DEAL/DealEncryptionRound.cs
@@ -1,11 +1,27 @@
+using CryptoLabs.DES;
 using CryptoLabs.Utility.Interfaces;
 
 namespace CryptoLabs.DEAL;
 
 public class DealEncryptionRound: IEncryptionRound
 {
+    private readonly DESAlgorithm _des;
+    private readonly DESKeyExpander _keyExpander = new();
+
+    public DealEncryptionRound()
+    {
+        byte[][] initialKeys = new byte[16][];
+        for (int i = 0; i < 16; i++)
+        {
+            initialKeys[i] = new byte[6];
+        }
+        _des = new DESAlgorithm(initialKeys);
+    }
+    
     public byte[] PerformEncryptConversion(byte[] inputBlock, byte[] roundKey)
     {
-        throw new NotImplementedException();
+        byte[][] desRoundKeys = _keyExpander.GenerateRoundKeys(roundKey);
+        _des.SetRoundKeys(desRoundKeys);
+        return _des.Encrypt(inputBlock);
     }
 }
