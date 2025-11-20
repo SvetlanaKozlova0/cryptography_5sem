1458faf8fea26439245a2e87ab2a7b74e8f5eb94
SvetlanaKozlova0 <lana.kozlova2005@list.ru>
2025-11-20 16:24:35 +0300

    box and rcon generators are done

---
+++ CryptoLabs/CryptoLabs/AES/Utility/AESBoxGenerator.cs
@@ -0,0 +1,77 @@
+namespace CryptoLabs.AES.Utility;
+using GF = CryptoLabs.Utility.MathUtils.GaloisFieldArithmetic;
+
+public class AESBoxGenerator
+{
+    private const int BoxLength = 256;
+    private const byte XorPoly = 0x63;
+    public byte Polynomial { get; }
+    private bool _wasGenerated;
+    private byte[] _sBox;
+    private byte[] _invBox;
+    
+    
+    public AESBoxGenerator(byte poly)
+    {
+        Polynomial = poly;
+    }
+    
+    
+    private (byte[] SBox, byte[] InvertedSBox) GenerateSBoxes()
+    {
+        var sBox = new byte[BoxLength];
+        var invertedBox = new byte[BoxLength];
+
+        for (var i = 0; i < BoxLength; i++)
+        {
+            var inverted = (i == 0) ? (byte) 0 : GF.Inverse((byte)i, Polynomial);
+            var affine = AffineTransformation(inverted);
+            var value = (byte)((affine ^ XorPoly) & 0xFF);
+            sBox[i] = value;
+            invertedBox[value & 0xFF] = (byte)i;
+        }
+        return (sBox, invertedBox);
+    }
+
+
+    public byte[] SBox()
+    {
+        if (_wasGenerated)
+        {
+            return _sBox;
+        }
+        (_sBox, _invBox) = GenerateSBoxes();
+        _wasGenerated = true;
+        return _sBox;
+    }
+
+    
+    public byte[] InvSBox()
+    {
+        if (_wasGenerated)
+        {
+            return _invBox;
+        }
+        (_sBox, _invBox) = GenerateSBoxes();
+        _wasGenerated = true;
+        return _invBox;
+    }
+
+    
+    private static byte ShiftLeftCycle(byte a, int shift)
+    {
+        int b = a & 0xFF;
+        return (byte)(((b << shift) | (b >> (8 - shift))) & 0xFF);
+    }
+
+    
+    private static byte AffineTransformation(byte b)
+    {
+        return (byte)(b ^
+                      (ShiftLeftCycle(b, 4)) ^
+                      (ShiftLeftCycle(b, 3)) ^ 
+                      (ShiftLeftCycle(b, 2)) ^ 
+                      (ShiftLeftCycle(b, 1))
+            );
+    }
+}
---
+++ CryptoLabs/CryptoLabs/AES/Utility/AESRconGenerator.cs
@@ -0,0 +1,28 @@
+namespace CryptoLabs.AES.Utility;
+using GF = CryptoLabs.Utility.MathUtils.GaloisFieldArithmetic;
+
+public class AESRconGenerator(in int nb, in int nr, in byte polynomial)
+{
+    private readonly int _nb = nb;
+    private readonly int _nr = nr;
+    private readonly byte _poly = polynomial;
+    
+    public byte[][] GenerateRCon()
+    {
+        var size = 4 * _nb * (_nr + 1);
+        var result = new byte[size][];
+        result[0] = new byte[4];
+        result[1] = new byte[4];
+        result[1][0] = 1;
+
+        for (int i = 2; i < size; i++)
+        {
+            byte previous = result[i - 1][0];
+            byte next = GF.OneMult(previous, _poly);
+            result[i] = new byte[4];
+            result[i][0] = next;
+        }
+
+        return result;
+    }
+}
