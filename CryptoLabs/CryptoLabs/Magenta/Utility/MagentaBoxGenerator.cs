7f81e51861ae94f6733c6ef9232f7d3dfbba8f67
SvetlanaKozlova0 <lana.kozlova2005@list.ru>
2025-11-23 20:13:18 +0300

    magenta s box generator

---
+++ CryptoLabs/CryptoLabs/Magenta/Utility/MagentaBoxGenerator.cs
@@ -0,0 +1,44 @@
+namespace CryptoLabs.Magenta.Utility;
+using GF = CryptoLabs.Utility.MathUtils.GaloisFieldArithmetic;
+
+public class MagentaBoxGenerator
+{
+    private const int BoxLength = 256;
+    private byte[] _sBox;
+    private bool _wasGenerated;
+    private byte Polynomial { get; }
+
+    
+    public MagentaBoxGenerator(byte poly)
+    {
+        Polynomial = poly;
+    }
+
+    
+    private byte[] GenerateSBox()
+    {
+        var sBox = new byte[BoxLength];
+        sBox[0] = 0x01;
+
+        for (var i = 1; i < BoxLength; i++)
+        {
+            sBox[i] = GF.OneMult(sBox[i - 1], Polynomial);
+        }
+
+        sBox[^1] = 0x00;
+
+        return sBox;
+    }
+    
+    
+    public byte[] SBox()
+    {
+        if (_wasGenerated)
+        {
+            return _sBox;
+        }
+        _sBox = GenerateSBox();
+        _wasGenerated = true;
+        return _sBox;
+    }
+}
