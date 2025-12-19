bd919119b56f4f3aa0619fe2382dfb9b294334b7
SvetlanaKozlova0 <lana.kozlova2005@list.ru>
2025-12-19 13:19:24 +0300

    rc4: file processing

--- CryptoLabs/CryptoLabs/RC4/RC4Algorithm.cs
+++ CryptoLabs/CryptoLabs/RC4/RC4Algorithm.cs
@@ -57,4 +57,77 @@ public class RC4Algorithm: IStreamCipher
         (_sBlock[_i], _sBlock[_j]) = (_sBlock[_j], _sBlock[_i]);
         return _sBlock[(_sBlock[_i] + _sBlock[_j]) % SBlockSize];
     }
+
+    
+    private static void ProcessFile(string input, string output, byte[] key, int bufferSize = 4096)
+    {
+        var rc4 = new RC4Algorithm();
+        rc4.Initialize(key);
+        using (var inputFile = new FileStream(input, FileMode.Open, FileAccess.Read))
+        using (var outputFile = new FileStream(output, FileMode.Create, FileAccess.Write))
+        {
+            var buffer = new byte[bufferSize];
+            int bytesRead;
+                
+            while ((bytesRead = inputFile.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                var bufferCopy = new byte[bytesRead];
+                Array.Copy(buffer, 0, bufferCopy, 0, bytesRead);
+                var processedData = rc4.Encrypt(bufferCopy);
+                outputFile.Write(processedData, 0, processedData.Length);
+            }
+        }
+    }
+
+    
+    public static void EncryptFile(string input, string output, byte[] key, int bufferSize = 4096)
+    {
+        ProcessFile(input, output, key, bufferSize);
+    }
+
+    
+    public static void DecryptFile(string input, string output, byte[] key, int bufferSize = 4096)
+    {
+        ProcessFile(input, output, key, bufferSize);
+    }
+    
+    
+    private static async Task ProcessFileAsync(string input, string output, byte[] key, 
+        int bufferSize = 4096, CancellationToken cancellationToken = default)
+    {
+        var rc4 = new RC4Algorithm();
+        rc4.Initialize(key);
+    
+        await using var inputFile = File.OpenRead(input);
+        await using var outputFile = File.Create(output);
+    
+        var buffer = new byte[bufferSize];
+        int bytesRead;
+    
+        while ((bytesRead = await inputFile.ReadAsync(buffer, cancellationToken)) > 0)
+        {
+            cancellationToken.ThrowIfCancellationRequested();
+            
+            for (int i = 0; i < bytesRead; i++)
+            {
+                buffer[i] ^= rc4.GetKeyStream();
+            }
+        
+            await outputFile.WriteAsync(buffer.AsMemory(0, bytesRead), cancellationToken);
+        }
+    }
+    
+    
+    public static Task EncryptFileAsync(string input, string output, byte[] key, 
+        int bufferSize = 4096, CancellationToken cancellationToken = default)
+    {
+        return ProcessFileAsync(input, output, key, bufferSize, cancellationToken);
+    }
+    
+    
+    public static Task DecryptFileAsync(string input, string output, byte[] key, 
+        int bufferSize = 4096, CancellationToken cancellationToken = default)
+    {
+        return ProcessFileAsync(input, output, key, bufferSize, cancellationToken);
+    }
 }
